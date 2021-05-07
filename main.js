10b8bea0dc916e44726c65dd3f0fd1c95ace5c54
ykangjune <83484640+ykangjune@users.noreply.github.com>
2021-05-07 09:54:42 +0900

    last drill - navbar
    writing html & css : good
    writing js : not good > need more time to study

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ const icons = document.querySelector('.navbar__icons');
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
-})
+});
