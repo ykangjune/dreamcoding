ae3fef3b00b31e78d913527bca427396e1ada04c
ykangjune <83484640+ykangjune@users.noreply.github.com>
2021-05-09 10:38:46 +0900

    Static Youtube Site
    a version

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
 });
